52b4e1bdbb5ace250e04561378c501844765dd99
Luciano <lucianodacunha.net@gmail.com>
2023-09-12 19:54:18 -0300

    Inclusão de classe que representa Conteudo Educional.

--- desafio/src/main/kotlin/net/lucianodacunha/desafio/main.kt
+++ desafio/src/main/kotlin/net/lucianodacunha/desafio/main.kt
@@ -18,3 +18,11 @@ enum class TipoDeConteudoEducacional {
 }
 
 class Usuario(val username: String, val nome: String)
+
+abstract class ConteudoEducacional(val nome: String, val duracao: Int,
+                               val nivel: Nivel,
+                               val tipo : TipoDeConteudoEducacional){
+    val inscritos = mutableSetOf<Usuario>()
+    abstract fun matricular(usuario: Usuario)
+    abstract fun cancelarMatricula(usuario : Usuario)
+}
